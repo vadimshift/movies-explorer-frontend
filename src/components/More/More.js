e0e1fb1b0876136938c36640e558f879fc1aef77
vadimshift <vadimshift@yandex.ru>
2021-07-27 16:41:06 +0500

    сверстал компонент more

---
+++ src/components/More/More.js
@@ -0,0 +1,11 @@
+import './More.css';
+
+function More() {
+  return (
+        <section className="more">
+            <button type="button" className="more__button">Ещё</button>
+        </section>
+  );
+}
+
+export default More;
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,14 +1,18 @@
 import './MoviesCardList.css';
 import MoviesCard from "../MoviesCard/MoviesCard";
+import More from "../More/More";
 
 function MoviesCardList() {
   return (
+      <div>
         <section className="movies-card-list">
             <MoviesCard />
             <MoviesCard />
             <MoviesCard />
             <MoviesCard />
         </section>
+        <More />
+    </div>
   );
 }
 
