b79460baa36f9d96c78b415478198c80584eb6d4
vadimshift <vadimshift@yandex.ru>
2021-07-31 15:48:28 +0500

    сделал адаптивной главную страницу

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,40 +6,44 @@ function AboutMe() {
   return (
     <section className='about-me' id='about-me'>
       <SectionTitle title='Студент' />
-      <img className='about-me__avatar' src={avatar} alt='Моя фотография' />
-      <h3 className='about-me__title'>Вадим</h3>
-      <h4 className='about-me__subtitle'>Фронтенд-разработчик, 31 год</h4>
-      <p className='about-me__text'>
-        Когда в офисе я всё починил и настроил, то понял, что пора сменить
-        сферу, иначе стану невостребованным. Веб-разработку выбрал, т.к. имел
-        небольшой опыт работы с HTML в студенческие годы. Живу за городом, в
-        собственном доме, поэтому всё свое свободное время я трачу на его
-        обустройство и на работу в саду. Недавно освоил навык работы со
-        сварочным оборудованием, изготовил калитку и ворота. В&nbsp;данный
-        момент осваиваю плотничное дело.
-      </p>
-      <ul className='about-me__links'>
-        <li>
-          <a
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://web.facebook.com/vadim.shift.9/'
-            className='about-me__link'
-          >
-            Facebook
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://github.com/vadimshift'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='about-me__link'
-          >
-            Github
-          </a>
-        </li>
-      </ul>
+      <div className='about-me__mega-container'>
+        <div className='about-me__container'>
+          <h3 className='about-me__title'>Вадим</h3>
+          <h4 className='about-me__subtitle'>Фронтенд-разработчик, 31 год</h4>
+          <p className='about-me__text'>
+            Когда в офисе я всё починил и настроил, то понял, что пора сменить
+            сферу, иначе стану невостребованным. Веб-разработку выбрал, т.к.
+            имел небольшой опыт работы с HTML в студенческие годы. Живу за
+            городом, в собственном доме, поэтому всё свое свободное время я
+            трачу на его обустройство и на работу в саду. Недавно освоил навык
+            работы со сварочным оборудованием, изготовил калитку и ворота.
+            В&nbsp;данный момент осваиваю плотничное дело.
+          </p>
+          <ul className='about-me__links'>
+            <li>
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://web.facebook.com/vadim.shift.9/'
+                className='about-me__link'
+              >
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://github.com/vadimshift'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='about-me__link'
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+        <img className='about-me__avatar' src={avatar} alt='Моя фотография' />
+      </div>
     </section>
   );
 }
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -3,7 +3,7 @@ import './Promo.css';
 function Promo(props) {
     return (
       <section className="promo">
-          <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+          <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
           <span className="promo__menu">
             {props.children}
           </span>
