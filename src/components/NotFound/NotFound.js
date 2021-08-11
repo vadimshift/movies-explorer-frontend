b55f38f7f24a63f3f27ba58cb0eaf50c4a98b967
vadimshift <vadimshift@yandex.ru>
2021-08-11 23:42:44 +0500

    notfound component complete

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,21 @@
 import './NotFound.css';
+import { Link, useHistory } from 'react-router-dom';
 
 function NotFound() {
+  const history = useHistory();
+
+  const goBack = () => {
+    history.goBack();
+  };
+
   return (
     <section className='not-found'>
-      <div className="not-found__container">
-      <h3 className='not-found__title'>404</h3>
-      <p className='not-found__subtitle'>Страница не найдена</p>
-      <button className='not-found__link'>Назад</button>
+      <div className='not-found__container'>
+        <h3 className='not-found__title'>404</h3>
+        <p className='not-found__subtitle'>Страница не найдена</p>
+        <Link onClick={goBack} className='not-found__link'>
+          Назад
+        </Link>
       </div>
     </section>
   );
